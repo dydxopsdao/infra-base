c6fda39d0021253541fb1f6d974283f0c8043efc
Rudi Mocnik <17800978+rudimocnik@users.noreply.github.com>
2024-07-09 21:49:43 +0200

    add var data to aws_organization, require ebs encryption, and deny unencrypted ebs scp, enable default ebs encryption for ap_northeast_1

--- aws_organization/main.tf
+++ aws_organization/main.tf
@@ -5,29 +5,31 @@ module "password_policy_dos_organization" {
   }
 }
 
+data "aws_organizations_organization" "org" {}
+
 module "prevent_leave_org_scp" {
   source = "./modules/aws_organization_scp"
   providers = {
-      aws = aws.org_level
-    }
+    aws = aws.org_level
+  }
   policy_name        = "PreventLeaveOrganization"
   policy_description = "Prevents accounts from leaving the organization"
   policy_statements = [
     {
-      effect    = "Deny"
-      actions   = ["organizations:LeaveOrganization"]
-      resources = ["*"]
+      effect     = "Deny"
+      actions    = ["organizations:LeaveOrganization"]
+      resources  = ["*"]
       conditions = []
     }
   ]
-  target_ids = var.prevent_leave_org_target_ids
+  target_ids = [data.aws_organizations_organization.org.roots[0].id]
 }
 
 module "region_restriction_scp" {
   source = "./modules/aws_organization_scp"
   providers = {
-      aws = aws.org_level
-    }
+    aws = aws.org_level
+  }
   policy_name        = "RegionRestriction"
   policy_description = "Restricts usage to approved regions"
   policy_statements = [
@@ -44,7 +46,55 @@ module "region_restriction_scp" {
       ]
     }
   ]
-  target_ids = var.region_restriction_target_ids
+  target_ids = [data.aws_organizations_organization.org.roots[0].id]
+}
+
+module "prevent_disable_ebs_encryption_scp" {
+  source = "./modules/aws_organization_scp"
+  providers = {
+    aws = aws.org_level
+  }
+  policy_name        = "PreventDisableEBSEncryption"
+  policy_description = "Prevents disabling default EBS encryption"
+  policy_statements = [
+    {
+      effect     = "Deny"
+      actions    = ["ec2:DisableEbsEncryptionByDefault"]
+      resources  = ["*"]
+      conditions = []
+    }
+  ]
+  target_ids = [data.aws_organizations_organization.org.roots[0].id]
+}
+
+module "deny_unencrypted_ebs_volumes_scp" {
+  source = "./modules/aws_organization_scp"
+  providers = {
+    aws = aws.org_level
+  }
+  policy_name        = "DenyUnencryptedEBSVolumes"
+  policy_description = "Denies creation of unencrypted EBS volumes"
+  policy_statements = [
+    {
+      effect    = "Deny"
+      actions   = ["ec2:CreateVolume"]
+      resources = ["*"]
+      conditions = [
+        {
+          test     = "Bool"
+          variable = "ec2:Encrypted"
+          values   = ["false"]
+        }
+      ]
+    }
+  ]
+  target_ids = [data.aws_organizations_organization.org.roots[0].id]
+}
+
+# Enable default EBS encryption for ap-northeast-1
+resource "aws_ebs_encryption_by_default" "ap_northeast_1" {
+  provider = aws.org_level
+  enabled  = true
 }
 
 # Other AWS organization-related resources and modules can be added here
--- aws_organization/variables.tf
+++ aws_organization/variables.tf
@@ -1,8 +1,3 @@
-variable "prevent_leave_org_target_ids" {
-  type        = list(string)
-  description = "Target IDs for the prevent leave organization SCP"
-}
-
 variable "allowed_global_actions" {
   type        = list(string)
   description = "List of allowed global actions for the region restriction SCP"
@@ -13,9 +8,5 @@ variable "approved_regions" {
   description = "List of approved AWS regions"
 }
 
-variable "region_restriction_target_ids" {
-  type        = list(string)
-  description = "Target IDs for the region restriction SCP"
-}
 
 # Other variables related to AWS organization management can be added here
