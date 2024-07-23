5e1dbf47767a6976f0c2424c22e7d4564853efee
Rudi Mocnik <17800978+rudimocnik@users.noreply.github.com>
2024-07-23 05:01:43 +0400

    fix(iam): remove terraformer IAM user from member accounts
    - Deleted terraformer IAM user, access keys and policy in all member accounts
    - Updated README.md to reflect migration to iam ic

--- aws_organization/dos_validator_snapshots.tf
+++ aws_organization/dos_validator_snapshots.tf
@@ -27,24 +27,6 @@ module "password_policy_dos_validator_snapshots" {
   }
 }
 
-module "dos_validator_snapshots_terraformer" {
-  source = "./modules/iam_user"
-  providers = {
-    aws = aws.dos_validator_snapshots
-  }
-  name        = "terraformer"
-  permissions = ["s3:*", "iam:*"]
-}
-
-output "dos_validator_snapshots_organization_id" {
-  value = aws_organizations_account.dos_validator_snapshots.id
-}
-
-output "dos_validator_snapshots_terraformer_outputs" {
-  value     = module.dos_validator_snapshots_terraformer
-  sensitive = true
-}
-
 # Integrate Terraform Cloud workspace with AWS using OpenID Connect (OIDC)
 module "tfc_oidc_dos_validator_snapshots" {
   source = "./modules/tfc_oidc"
